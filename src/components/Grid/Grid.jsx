2d697861cfe80551e0af1479d4243aa8be2fab42
Divyanshu Sahu <257divyanshu@gmail.com>
2025-07-17 17:28:23 +0530

    feat : implement winner detection logic

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -2,10 +2,36 @@ import { useState } from "react";
 import Card from "../Card/Card";
 import "./Grid.css";
 
+function isWinner(board, symbol){
+    if(
+        (board[0]===board[1] && board[1]===board[2] && board[2]===symbol)
+        ||
+        (board[3]===board[4] && board[4]===board[5] && board[5]===symbol)
+        ||
+        (board[6]===board[7] && board[7]===board[8] && board[8]===symbol)
+        ||
+        (board[0]===board[4] && board[4]===board[8] && board[8]===symbol)
+        ||
+        (board[2]===board[4] && board[4]===board[6] && board[6]===symbol)
+        ||
+        (board[0]===board[3] && board[3]===board[6] && board[6]===symbol)
+        ||
+        (board[1]===board[4] && board[4]===board[7] && board[7]===symbol)
+        ||
+        (board[2]===board[5] && board[5]===board[8] && board[8]===symbol)
+    ){
+        return symbol;
+    }
+    else{
+        return null;
+    }
+}
+
 function Grid({ numberOfCards = 9 }) {
     // console.log('grid re-rendered');
     let [turn, setTurn] = useState(true); // false -> X true -> O
     let [board, setBoard] = useState(Array(numberOfCards).fill(""));
+    let [winner, setWinner] = useState(null);
     function play(index){
         // console.log(index,'clicked');
         if(turn){
@@ -13,13 +39,18 @@ function Grid({ numberOfCards = 9 }) {
         }
         else{
             board[index] = "X";
-        }
+        };
+        const win = isWinner(board,turn ? "O" : "X");
+        if(win){
+            setWinner(win);
+            console.log(win,'has won the game!');
+        };
         setTurn(!turn);
         setBoard([...board]);
-        return turn;
     }
     return (
         <>
+            {winner && <h1 className="turn-highlight">{winner} is the winner</h1>}
             <h1 className="turn-highlight">
                 Current Turn : {turn ? "O":"X"}
             </h1>
