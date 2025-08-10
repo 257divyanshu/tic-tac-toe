8ceff1cbfc7b2a78c1fb2e992506ef10d63b6bf2
Divyanshu Sahu <257divyanshu@gmail.com>
2025-08-10 14:18:07 +0530

    feat : implement complete UI redesign and styling

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Card from "../Card/Card";
 import "./Grid.css";
-import {toast, ToastContainer} from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/ReactToastify.css";
 import isWinner from "../../helpers/checkWinner";
 
@@ -9,9 +9,10 @@ function Grid({ numberOfCards = 9 }) {
     // console.log('grid re-rendered');
     let [turn, setTurn] = useState(true); // false -> X true -> O
     let [board, setBoard] = useState(Array(numberOfCards).fill(""));
+    let [checked, setChecked] = useState(0);
     let [winner, setWinner] = useState(null);
     function play(index) {
-        // console.log(index,'clicked');
+        setChecked(checked + 1);
         if (turn) {
             board[index] = "O";
         }
@@ -21,34 +22,50 @@ function Grid({ numberOfCards = 9 }) {
         const win = isWinner(board, turn ? "O" : "X");
         if (win) {
             setWinner(win);
-            toast.success(`Congratulations ${win}! You have won the game!`);            
+            toast.success(`Congratulations ${win}! You have won the game!`);
         };
         setTurn(!turn);
         setBoard([...board]);
     }
-    function resetGame(){
+    function resetGame() {
         setBoard(Array(numberOfCards).fill(""));
         setWinner(null);
         setTurn(true);
+        setChecked(0);
     }
     return (
         <div className="grid-wrapper">
-            {winner && (
-                <>
-                    <h1 className="turn-highlight">{winner} is the winner</h1>
-                    <button
-                        className="reset"
-                        onClick={resetGame}
-                    >Reset Game</button>
-                </>
-            )}
-            <h1 className="turn-highlight">
+            {!winner && checked!=9 && <h1
+                className="displayer"
+            >
                 Current Turn : {turn ? "O" : "X"}
-            </h1>
+            </h1>}
+            {!winner && checked==9 && <h1
+                className="displayer"
+            >
+                It's a draw
+            </h1>}
+            {winner && <h1
+                className="displayer"
+                style={{
+                    backgroundColor: winner==='O' ? "#f08080" : "#6ca9f0"
+                }}
+            >
+                {winner} is the winner!
+            </h1>}
             <div className="grid">
                 {board.map((elem, idx) => <Card key={idx} gameEnd={winner ? true : false} player={elem} index={idx} onPlay={play} />)}
             </div>
-            <ToastContainer position="top-center"/>
+            {(winner || checked == 9) && (
+                <div className="reset-btn-holder">
+                    <button
+                        className="displayer reset-btn"
+                        onClick={resetGame}
+                    >
+                        Reset Game
+                    </button>
+                </div>
+            )}
         </div>
     )
 }
--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,14 +1,32 @@
-import {FaTimes, FaRegCircle, FaPen} from "react-icons/fa"
+import "./icon.css";
 
 function Icon({iconName}){
+    // console.log('Icon rendered')
     if(iconName==="circle"){
-        return <FaRegCircle/>
+        return <div className="icons" style={{
+            borderColor: "#f08080"
+        }}>
+            <svg 
+                className="o-icon"
+                viewBox="0 0 100 100">
+              <circle cx="50" cy="50" r="35" strokeWidth="12" fill="none" />
+            </svg>
+        </div>
     }
     else if (iconName==="cross"){
-        return <FaTimes/>
+        return <div className="icons" style={{
+            borderColor: "#6ca9f0"
+        }}>
+            <svg 
+                className="x-icon"
+                viewBox="0 0 100 100">
+                <line x1="20" y1="20" x2="80" y2="80" strokeWidth="15" strokeLinecap="round" />
+                <line x1="80" y1="20" x2="20" y2="80" strokeWidth="15" strokeLinecap="round" />
+            </svg>
+        </div>
     }
     else{
-        return <FaPen/>
+        return <div className="icons"></div>
     }
 }
 
